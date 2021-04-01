c57cd727d374c71dfcf3c6c42ed8e9b5be75e7f6
Joel Eckert <joel@joelmeckert.com>
2021-03-31 23:33:30 -0400

    Minor code and comment cleanup, verbosity level

--- WinDnsToUnbound.ps1
+++ WinDnsToUnbound.ps1
@@ -1,18 +1,18 @@
-#Requires -runasadministrator
+#Requires -RunAsAdministrator
 #Requires -Version 4
 <#
 .SYNOPSIS
-  Gets the DNS zone information and records from Active Directory, exports it to an include file for the Unbound DNS resolver.
+  Obtains the DNS zones and records from Active Directory, exports to an include file for the Unbound DNS resolver, opens the file with Notepad / Notepad=++ for viewing / copy/paste
 .DESCRIPTION
   Rationale:
-  * DNS needs appliance-level availability, and forcing Windows DNS with Active Directory impacts uptime and perception, server down == network down
+  * DNS needs appliance-level availability, and forcing Windows DNS with Active Directory impacts uptime, with the perception of server down == network down
   * This provides for the potential of local host resolution using the Active Directory / Azure AD domain, and retaining UPNs that match the Azure AD, e.g. email@contoso.com
   * The Unbound option of TypeTransparent will attempt to resolve the DNS query, and if there is no entry, it will pass it on to the host / firewall's DNS servers
   * pfSense's DHCP implementation will automatically register DNS and PTR
   pfSense configuration, as custom options:
   * Copy the contents of the newly created file
   * Open the pfSense web interface, browse to Services, DNS Resolver, Custom Options, and paste the entries
-  * Backed up with the pfSense configuration file
+  * This method allows for a full pfSense restore using the configuration file only
   pfSense, as an include file:
   * Upload the file to /var/unbound/unbound.adinclude.conf ...
   * Open the pfSense web interface, browse to Services, DNS Resolver, Custom Options
@@ -24,13 +24,13 @@
   # [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters]
   # "UseDynamicDns"=dword:00000000
 .PARAMETER OutputFile
-  Output filename, default is ".\unbound.adinclude.conf", removes any file that exists
+  Output filename, default is "unbound.adinclude.conf" in the PowerShell script directory, removes any file that exists
 .INPUTS
   None
 .OUTPUTS
   Configuration file, as per above
 .NOTES
-  Version:        0.0.2
+  Version:        0.0.2.1
   Author:         Joel Eckert
   Creation Date:  November 15, 2020
   Purpose/Change: Initial script development
@@ -69,17 +69,16 @@ Catch {
     Exit
 }
 
-Write-Host "Processing. . ."
 ForEach ($zone in $zones) {
     Try {
         $resourceRecords = Get-DnsServerResourceRecord -ZoneName $zone
     }
     Catch {
-        Write-Error "Unable to obtain DNS resource records for $zone"
+        Write-Error "Zone: $zone DNS resource records lookup failed"
         Start-Sleep 5
         Continue
     }
-    Write-Host "Zone: $zone"
+    Write-Verbose "Zone: $zone DNS resource records lookup successful"
     # Entries for new zones in the Unbound configuration file
     $zoneEntry = 'local-zone: "' + $zone + "`"" + " " + "typetransparent" + "`n"
     $zoneEntry += 'private-domain: "' + $zone + "`"" + "`n"
@@ -159,15 +158,14 @@ ForEach ($zone in $zones) {
 # Replace the Windows carriage returns
 (Get-Content $OutputFile -Raw).Replace("`r`n","`n").Trim() | Set-Content $OutputFile -Force
 
-Write-Host ""
-Write-Host "Output conf file w/o CR: $OutputFile"
-Start-Sleep 5
+Write-Verbose "UNIX output file: $OutputFile"
 
 # Launch Notepad++ or Notepad, if not installed, with the configuration file
 $NotepadPlusPlus = "C:\Program Files\Notepad++\notepad++.exe"
+$Notepad = Join-Path $PSScriptRoot "System32\notepad.exe"
 If (Test-Path $NotepadPlusPlus) {
     & $NotepadPlusPlus $OutputFile
 }
 Else {
-    & "notepad.exe" $OutputFile
+    & $Notepad $OutputFile
 }
